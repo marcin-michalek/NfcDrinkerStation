8e29425dd32f7860837840cbd573364bd7f8657e
Marcin Michałek <marcin.marcim@gmail.com>
2014-12-31 18:01:11 +0100

    project init

---
+++ app/src/main/java/pl/michalek/marcin/nfcdrinkerstation/BaseActivity.java
@@ -0,0 +1,16 @@
+/**
+ * Created by MiQUiDO on 2014-12-31.
+ *
+ * Copyright 2014 MiQUiDO <http://www.miquido.com/>. All rights reserved.
+ */
+package pl.michalek.marcin.nfcdrinkerstation;
+
+import android.app.Activity;
+
+/**
+ * TODO Add class description...
+ *
+ * @author miquido
+ */
+public class BaseActivity extends Activity {
+}
